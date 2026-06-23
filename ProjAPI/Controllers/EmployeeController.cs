eb4b16ddd583e10148a147f5359c6f58b52528fc
Matheus Rodrigues Medeiros <matheusrodrigues03.contato@gmail.com>
2026-06-23 10:44:49 -0300

    Delete ProjAPI directory

--- ProjAPI/Controllers/EmployeeController.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace ProjAPI;
-
-public class EmployeeController : Controller
-{
-}
--- ProjAPI/Employees.cs
+++
@@ -1,9 +0,0 @@
-public class Employees
-{
-    public int Id { get; set; }
-    public int Idade { get; set; }
-    public required string PrimeiroNome { get; set; }
-    public required string UltimoNome { get; set; }
-
-
-}
