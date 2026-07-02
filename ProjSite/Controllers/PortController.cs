2f471e577204e826bcc6ed6c7174d0da4c0e02e1
Matheus <matheus.rod03@gmail.com>
2026-07-02 13:10:25 -0300

    Commit do ProjSite

---
+++ ProjSite/Controllers/PortController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjSite.Controllers
+{
+    public class PortController : Controller
+    {
+        // GET: PortController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
