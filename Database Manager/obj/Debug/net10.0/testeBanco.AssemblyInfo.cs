fbe52aa08a6f271eb712fad8cf08a667e6d8ba19
Matheus <matheus.rod03@gmail.com>
2026-08-24 14:52:51 -0300

    atualizando versões informativas nos arquivos AssemblyInfo

--- Database Manager/obj/Debug/net10.0/testeBanco.AssemblyInfo.cs
+++ Database Manager/obj/Debug/net10.0/testeBanco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testeBanco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d857bc37690c3198e06bc13fc35e4f336d122043")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78188227fdd03a6ab3ecbbfebb9bc94e75dfc62f")]
 [assembly: System.Reflection.AssemblyProductAttribute("testeBanco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testeBanco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
